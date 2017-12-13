681cc8d4841ab1441d27993ab86885875baa5db2
Andy Maskiell <maskiella@gmail.com>
2017-12-13 19:27:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alan_kay[:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 		 programmer_hash
 
 
@@ -128,4 +128,5 @@ def adding_to_dennis
         }
      }
 
+		 programmer_hash
 end
